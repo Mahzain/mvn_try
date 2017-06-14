aeea75dde85bb1ff480b3cc819c585d138e31c08
mzn <mahzainmalik@gmail.com>
2017-06-14 14:35:19 +0500

    mymsg

---
+++ src/main/java/com/mzn/App.java
@@ -0,0 +1,17 @@
+package com.mzn;
+
+/**
+ * Hello world!
+ *
+ */
+
+import com.google.gson.*;
+import com.fasterxml.jackson.core.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
